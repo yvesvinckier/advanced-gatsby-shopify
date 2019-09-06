b4b00a911d279aafd94cf734199735d45d168bef
Yves Vinckier <yves.vinckier@live.fr>
2019-09-06 22:20:03 +0200

    wrapping Project root

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,13 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from 'react'
+import { StoreProvider } from './src/context/StoreContext'
+
+export const wrapRootElement = ({ element }) => (
+    <StoreProvider>
+        {element}
+    </StoreProvider>
+
+)
