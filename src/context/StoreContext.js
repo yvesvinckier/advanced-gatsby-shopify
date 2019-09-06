d4e1cc01415c291497287ee95f11c28957ffb882
Yves Vinckier <yves.vinckier@live.fr>
2019-09-06 22:39:26 +0200

    shopify buy client

--- src/context/StoreContext.js
+++ src/context/StoreContext.js
@@ -1,9 +1,19 @@
 import React, { createContext, useState } from 'react'
 
+import Client from 'shopify-buy'
+
+const client = Client.buildClient({
+    domain: "advanced-gatsby-blowup.myshopify.com",
+    storefrontAccessToken: "3c3a9ad7c0d62b77fabe8ff90e18f816",
+})
+
 const defaultValues = {
     isCartOpen: false,
     cart: [],
-    addProductToCart: () => { console.log('added!') }
+    addProductToCart: () => {
+        console.log('added!')
+    },
+    client
 }
 
 
