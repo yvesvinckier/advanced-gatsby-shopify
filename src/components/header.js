63515bbef69fc624229e102c88ce4bbbf6c8d40f
Yves Vinckier <yves.vinckier@live.fr>
2019-09-12 23:21:04 +0200

    Loading Screens

--- src/components/header.js
+++ src/components/header.js
@@ -7,9 +7,12 @@ import "../style.scss"
 import { StoreContext } from "../context/StoreContext"
 import logo from "../images/logo.svg"
 import Cart from "./Cart/Cart"
+import Loader from "./Loader"
 
 const Header = ({ siteTitle }) => {
-  const { isCartOpen, toggleCartOpen, checkout } = useContext(StoreContext)
+  const { isCartOpen, toggleCartOpen, checkout, isLoading } = useContext(
+    StoreContext
+  )
   const transitions = useTransition(isCartOpen, null, {
     from: { transform: "translate3d(100%, 0, 0)" },
     enter: { transform: "translate3d(0, 0, 0)" },
@@ -19,56 +22,61 @@ const Header = ({ siteTitle }) => {
     return total + item.quantity
   }, 0)
   return (
-    <header
-      className="level is-mobile"
-      style={{ background: "var(--purp)", boxShadow: "var(--elevation-2)" }}
-    >
-      <div className="level-left">
-        <Link to="/" className="navbar-item">
-          <img
-            style={{ height: 60, maxHeight: "none", marginBottom: 0 }}
-            src={logo}
-            alt="Level Up Logo"
-          />
-        </Link>
-      </div>
-      <div className="level-right">
-        <div className="navbar-item">
-          <button
-            className="button"
-            style={{
-              position: "relative",
-              background: "transparent",
-              border: "none",
-            }}
-            onClick={toggleCartOpen}
-          >
-            {qty > 0 && (
-              <div
-                style={{
-                  color: "white",
-                  position: "absolute",
-                  background: "var(--red)",
-                  borderRadius: 15,
-                  textAlign: "center",
-                  height: 30,
-                  top: -5,
-                  right: -5,
-                  width: 30,
-                  lineHeight: "30px",
-                }}
-              >
-                {qty}
-              </div>
-            )}
-            <FaShoppingCart style={{ color: "white", height: 30, width: 30 }} />
-          </button>
+    <>
+      <header
+        className="level is-mobile"
+        style={{ background: "var(--purp)", boxShadow: "var(--elevation-2)" }}
+      >
+        <div className="level-left">
+          <Link to="/" className="navbar-item">
+            <img
+              style={{ height: 60, maxHeight: "none", marginBottom: 0 }}
+              src={logo}
+              alt="Level Up Logo"
+            />
+          </Link>
         </div>
-      </div>
-      {transitions.map(
-        ({ item, key, props }) => item && <Cart key={key} style={props} />
-      )}
-    </header>
+        <div className="level-right">
+          <div className="navbar-item">
+            <button
+              className="button"
+              style={{
+                position: "relative",
+                background: "transparent",
+                border: "none",
+              }}
+              onClick={toggleCartOpen}
+            >
+              {qty > 0 && (
+                <div
+                  style={{
+                    color: "white",
+                    position: "absolute",
+                    background: "var(--red)",
+                    borderRadius: 15,
+                    textAlign: "center",
+                    height: 30,
+                    top: -5,
+                    right: -5,
+                    width: 30,
+                    lineHeight: "30px",
+                  }}
+                >
+                  {qty}
+                </div>
+              )}
+              <FaShoppingCart
+                style={{ color: "white", height: 30, width: 30 }}
+              />
+            </button>
+          </div>
+        </div>
+        {transitions.map(
+          ({ item, key, props }) => item && <Cart key={key} style={props} />
+        )}
+      </header>
+      <Loader />
+    </>
   )
 }
 
